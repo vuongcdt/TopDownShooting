f2bd81c408bff1542af4dd77878da9c1c88a5990
Vuongvg <vuongcdt@gmail.com>
2024-05-18 02:14:44 +0700

    update velocity player

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -8,25 +8,24 @@ namespace Scritps
     public class TouchController : MonoBehaviour
     {
         [SerializeField] private float threshHold = 0.2f;
-        [SerializeField] private float velocityPlayer = 0.2f;
         [SerializeField] private Camera mainCamera;
         [SerializeField] private GameObject anim;
-        private Rigidbody2D _rigidbody2DAnim;
-        [FormerlySerializedAs("velcityLimit")] [SerializeField] private int velocityLimit = 2;
+        [SerializeField] private int velocityLimit = 2;
 
+        private Rigidbody2D _rigidbody2DAnim;
+        private Animator _animator;
         private float _timeHold;
+        private static readonly int Run = Animator.StringToHash("run");
 
         private void Start()
         {
             _rigidbody2DAnim = anim.GetComponent<Rigidbody2D>();
+            _animator = anim.GetComponent<Animator>();
+            Debug.Log(Run);
         }
 
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-            }
-
             if (Input.GetMouseButtonUp(0))
             {
                 _timeHold = 0;
@@ -38,18 +37,23 @@ namespace Scritps
             _timeHold += Time.deltaTime;
             if (_timeHold >= threshHold)
             {
+                _animator.SetTrigger(Run);
+                var positionAnim = anim.transform.position;
+
                 var mousePos = Input.mousePosition;
                 var worldPos = mainCamera.ScreenToWorldPoint(mousePos);
+                var distanceToMouse = Vector2.Distance(worldPos, positionAnim);
 
-                var position = anim.transform.position;
-
-                var velocity = (Vector2)worldPos - (Vector2)position;
+                var scaleVelocity = distanceToMouse > 0.2 ? velocityLimit / distanceToMouse : 1;
+                
+                Vector2 velocity = (worldPos - positionAnim) * scaleVelocity;
 
                 velocity.x = Mathf.Clamp(velocity.x, -velocityLimit, velocityLimit);
                 velocity.y = Mathf.Clamp(velocity.y, -velocityLimit, velocityLimit);
 
-                _rigidbody2DAnim.velocity = new Vector2(velocity.x, velocity.y);
+                _rigidbody2DAnim.velocity = velocity;
 
+                Debug.Log($"diatanceToMouse: {distanceToMouse}");
                 Debug.Log($"_rigidbody2DAnim.velocity: {_rigidbody2DAnim.velocity}");
             }
         }
